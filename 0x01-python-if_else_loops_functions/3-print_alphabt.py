640c38a4fb6e1bfe5611f1275cbc358eb561eec2
Noahxyzs <noahsolomon528@yahoo.com>
2023-03-07 06:17:25 -0800

    if else loop on python

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 for a in range(ord("a"), ord("z") + 1):
-    if chr(a) is not "q" and chr(a) is not "e":
+    if chr(a) != "e" and chr(a) != "q":
         print("{:s}".format(chr(a)), end="")
