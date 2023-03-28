80b25274ea0c1c1587810e93a21719c4278a63cb
Noahxyzs <noahsolomon528@yahoo.com>
2023-03-28 09:20:31 -0700

    classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,18 @@
 #!/usr/bin/python3
-class Square():
-    """ A square class """
+"""Define a class Square."""
+
+
+class Square:
+    """Represent a square."""
+
     def __init__(self, size=0):
-        """ Initializing class """
-        self.__size = size
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
-        if size < 0:
+        elif size < 0:
             raise ValueError("size must be >= 0")
+        self.__size = size
