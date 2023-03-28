bb55b9da1d078bda4dfbb4bff56ce392a9653a54
Noahxyzs <noahsolomon528@yahoo.com>
2023-03-28 09:09:50 -0700

    classes

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-
 class Square():
+    """ A square class """
     def __init__(self, size=0):
-
+        """ Initializing class """
         self.__size = size
-
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
--- 0x06-python-classes/5-square.py
+++ 0x06-python-classes/5-square.py
@@ -1,19 +1,18 @@
 #!/usr/bin/python3
-
 class Square():
-
+    """ A square class """
     def __init__(self, size=0):
-
+        """ Initializing class """
         self.__size = size
 
     @property
     def size(self):
-
+        """ Property size """
         return self.__size
 
     @size.setter
     def size(self, value):
-
+        """ property setter size """
         if not isinstance(value, int):
             raise TypeError("size must be an integer")
         if value < 0:
@@ -21,11 +20,11 @@ class Square():
         self.__size = value
 
     def area(self):
-
+        """ Initializing area calc """
         return self.__size * self.__size
 
     def my_print(self):
-
+        """ Initializing printing square """
         if self.__size <= 0:
             print()
 
