8561137f0b12c00b6cf91f2a625c761184539da8
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-03 14:31:02 -0700

    rectangle

--- 0x08-python-more_classes/101-nqueens.py
+++ 0x08-python-more_classes/101-nqueens.py
@@ -1,106 +1,52 @@
 #!/usr/bin/python3
-"""
-class rectangle
-"""
+'''Module for N Queens problem.'''
 
 
-class Rectangle:
-    """
-    rectangle
-    """
-    number_of_instances = 0
-    print_hash = "#"
+def isSafe(board, row, col):
+    '''Checks if position is safe from attack.
 
-    def __init__(self, width=0, height=0):
-        """intializaion dender method
-        """
-        self.width = width
-        self.height = height
-        Rectangle.number_of_instances += 1
+    Args:
+        board: The board state.
+        row: The row to check.
+        col: The colum to check.
+    '''
+    for c in range(col):
+        if board[c] is row or abs(board[c] - row) is abs(c - col):
+            return False
+    return True
 
-    @property
-    def width(self):
-        """Getter get width
-        """
-        return self.__width
 
-    @width.setter
-    def width(self, value):
-        """Setter set width
-        """
-        if type(value) != int:
-            raise TypeError("width must be an integer")
-        if value < 0:
-            raise ValueError("width must be >= 0")
-        self.__width = value
+def checkBoard(board, col):
+    '''Checks the board state column by column using backtracking.
 
-    @property
-    def height(self):
-        """Getter get height
-        """
-        return self.__height
+    Args:
+        board: The board state.
+        col: The current colum to check.
+    '''
+    n = len(board)
+    if col is n:
+        print(str([[c, board[c]] for c in range(n)]))
+        return
 
-    @height.setter
-    def height(self, value):
-        """Setter set height
-        """
-        if type(value) != int:
-            raise TypeError("height must be an integer")
-        if value < 0:
-            raise ValueError("height must be >= 0")
-        self.__height = value
+    for row in range(n):
+        if isSafe(board, row, col):
+            board[col] = row
+            checkBoard(board, col + 1)
 
-    def area(self):
-        """Returns area
-        """
-        return self.__width * self.__height
+if __name__ == "__main__":
+    import sys
 
-    def perimeter(self):
-        """Returns perimeter
-        """
-        if self.__width == 0 or self.__height == 0:
-            return 0
-        else:
-            return 2 * (self.__width + self.__height)
-
-    def __str__(self):
-        """dendor str method """
-        if self.__width == 0 or self.__height == 0:
-            return ""
-        else:
-            string = ""
-            for a in range(self.__height):
-                if a == self.__height - 1:
-                    string += self.__width * str(self.print_hash)
-                else:
-                    string += self.__width * str(self.print_hash) + '\n'
-            return string
-
-    def __repr__(self):
-        """Returns str"""
-        return "Rectangle({:d}, {:d})".format(self.__width, self.__height)
-
-    def __del__(self):
-        """ magic delete method """
-        Rectangle.number_of_instances -= 1
-        print("Bye rectangle...")
-
-    @staticmethod
-    def bigger_or_equal(rect_1, rect_2):
-        """ params rect_1, rect_2"""
-        if not isinstance(rect_1, Rectangle):
-            raise TypeError("rect_1 must be an instance of Rectangle")
-        elif not isinstance(rect_2, Rectangle):
-            raise TypeError("rect_2 must be an instance of Rectangle")
-        elif rect_1.area() >= rect_2.area():
-            return rect_1
-        else:
-            return rect_2
-
-    @classmethod
-    def square(cls, size=0):
-        """returns a new Rectangle instance
-        """
-        height = size
-        width = size
-        return cls(height, width)
+    if len(sys.argv) != 2:
+        print("Usage: nqueens N")
+        sys.exit(1)
+    n = 0
+    try:
+        n = int(sys.argv[1])
+    except:
+        print("N must be a number")
+        sys.exit(1)
+    if n < 4:
+        print("N must be at least 4")
+        sys.exit(1)
+    board = [0 for col in range(n)]
+    checkBoard(board, 0)
