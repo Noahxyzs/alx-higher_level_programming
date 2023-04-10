0383062bd2ea129153a07f0887b6c9c7d38b9d5e
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-10 07:26:00 -0700

    inheritance

---
+++ 0x0A-python-inheritance/8-rectangle.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+'''rectangle'''
+
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Rectangle(BaseGeometry):
+    '''create class'''
+    def __init__(self, width, height):
+        ''' init '''
+        self.integer_validator("width", width)
+        self.integer_validator("height", height)
+        self.__widht = width
+        self.__height = height
