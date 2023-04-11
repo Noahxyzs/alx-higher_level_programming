dc1f067c7c0c150a522f16141c1afc2fbb36cb56
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-11 11:39:13 -0700

    input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -9,7 +9,7 @@ save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 filename = "add_item.json"
 try:
     content = load_from_json_file(filename)
-except: Exception as e
+except Exception as e:
     content = []
 
 for i in range(1, len(argv)):
