d2c750a9c2d2a302476d6fbcfe97f4db68991bd0
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-11 11:38:16 -0700

    input/output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,25 +1,17 @@
 #!/usr/bin/python3
-''' add_item '''
-import sys
-save_to_json = __import__("5-save_to_json_file").save_to_json_file
-load_from_json = __import__("6-load_from_json_file").load_from_json_file
+from sys import argv
+"""access"""
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+"""create"""
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+"""writes"""
 
+filename = "add_item.json"
+try:
+    content = load_from_json_file(filename)
+except: Exception as e
+    content = []
 
-def add_item(args="", filename=""):
-    '''
-    add item
-    '''
-    try:
-        allArgs = load_from_json(filename)
-    except Exception as e:
-        allArgs = []
-
-    for item in args:
-        allArgs.append(item)
-    save_to_json(allArgs, filename)
-
-
-if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+for i in range(1, len(argv)):
+    content.append(argv[i])
+save_to_json_file(content, filename)
