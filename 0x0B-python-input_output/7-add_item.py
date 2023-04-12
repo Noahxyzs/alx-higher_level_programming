287bde43a495a83a99e9f7e83ad1c31b2a8cfc6d
Noahxyzs <noahsolomon528@yahoo.com>
2023-04-12 10:48:14 -0700

    input output

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,19 +1,17 @@
 #!/usr/bin/python3
+""" Module """
 import json
 from sys import argv
-"""
-function that add and save
-"""
 
 
-save_json = __import__('5-save_to_json_file').save_to_json_file
-load_json = __import__('6-load_from_json_file').load_from_json_file
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 
 filename = 'add_item.json'
 
 try:
     new = load_json(filename)
-except Exception as e:
+except (ValueError, FileNotFoundError):
     new = []
 
 new = new + argv[1:]
