98859a8407084f0e28d86516482d7792ce8e2a3c
Noahxyzs <noahsolomon528@yahoo.com>
2023-06-23 01:20:45 -0700

    hell

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -1,44 +1,17 @@
 #!/usr/bin/python3
-"""Display cities"""
+"""  lists all states from the database hbtn_0e_0_usa """
 import MySQLdb
-from sys import argv
+import sys
 
 
-def filter__list():
-    """Takes arguments argv to list from database
-    Only lists with states that matches name argument
-
-    Arguments:
-        argv[1]: mysql username
-        argv[2]: mysql password
-        argv[3]: database name
-    """
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3],
-                         charset="utf8",
-                         )
-
-    # Getting a cursor in MySQLdb python
+if __name__ == "__main__":
+    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
+                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
     cur = db.cursor()
-
-    # Executing db queries
-    cur.execute("SELECT cities.id, cities.name, states.name FROM cities\
-                INNER JOIN states ON cities.state_id = states.id\
-                ORDER BY cities.id ASC")
-
-    # fetches all the rows of a query result
-    query_rows = cur.fetchall()
-
-    # Printing the result one in one
-    for row in query_rows:
+    cur.execute("""SELECT cities.id, cities.name, states.name FROM
+                cities INNER JOIN states ON states.id=cities.state_id""")
+    rows = cur.fetchall()
+    for row in rows:
         print(row)
-
     cur.close()
     db.close()
-
-
-if __name__ == '__main__':
-    filter__list()
