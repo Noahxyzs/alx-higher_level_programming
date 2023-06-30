6f1d1cfa67f75b82a292c407da6f8c2e14b1d807
Noahxyzs <noahsolomon528@yahoo.com>
2023-06-30 09:30:28 -0700

    python net

--- 0x11-python-network_1/1-hbtn_header.py
+++ 0x11-python-network_1/1-hbtn_header.py
@@ -1,15 +1,12 @@
 #!/usr/bin/python3
-"""initializate"""
-from urllib.request import urlopen
-from sys import argv
-
-
-def hbtn_header_1():
-    """sends a request to the URL and
-    displays the value of the X-Request-Id"""
-    with urlopen(argv[1]) as response:
-        print(response.headers.get('X-Request-Id'))
-
-
-if __name__ == '__main__':
-    hbtn_header_1()
+"""script for testing status of web pages
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import sys
+    url = sys.argv[1]
+    with urllib.request.urlopen(url) as response:
+        meta = response.info()
+        for header in meta._headers:
+            if header[0] == 'X-Request-Id':
+                print(header[1])
