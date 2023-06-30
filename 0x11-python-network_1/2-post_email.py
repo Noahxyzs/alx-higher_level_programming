b0f55b53cf9bd4b364e0131e699e0ed6956c2d96
Noahxyzs <noahsolomon528@yahoo.com>
2023-06-30 10:10:54 -0700

    python net

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,21 +1,15 @@
 #!/usr/bin/python3
-"""initializate"""
-from sys import argv
-from urllib
-
-
-def post_email():
-    """ script that takes in a URL and an email, sends a POST
-    request to the passed URL with the email as a parameter,
-    and displays the body of the response (decoded in utf-8)"""
-    url = argv[1]
-    email = {'email': argv[2]}
-
-    data = urlencode(email).encode('utf-8')
-    req = Request(url, data)
-    with urlopen(req) as response:
+"""script for testing POST requests so servers
+"""
+if __name__ == "__main__":
+    import urllib.request
+    import urllib.parse
+    import sys
+    url = sys.argv[1]
+    email = sys.argv[2]
+    payload = {'email': email}
+    payload = urllib.parse.urlencode(payload)
+    payload = payload.encode('ascii')
+    req = urllib.request.Request(url, payload)
+    with urllib.request.urlopen(req) as response:
         print(response.read().decode('utf-8'))
-
-
-if __name__ == '__main__':
-    post_email()
