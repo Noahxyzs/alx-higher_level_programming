e91cff90839d815419f59ac1ce6cfe9e33fab041
Noahxyzs <noahsolomon528@yahoo.com>
2023-06-30 04:07:58 -0700

    python net

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# script that was fun
-curl -s -L -X PUT -d "user_id=98" -H "Origin: You got me!i" 0.0.0.0:5000/catch_me
+# script to make req
+curl -sL 0.0.0.0:5000/catch_me_3 -X PUT -d "user_id=98" -H "Origin:School"
