eecaf6d582b7f1ce3c53513c6b25d6e3b35eb957
Noahxyzs <noahsolomon528@yahoo.com>
2023-07-25 11:51:00 -0700

    java

--- 0x14-javascript-web_scraping/100-starwars_characters.js
+++ 0x14-javascript-web_scraping/100-starwars_characters.js
@@ -1,15 +1,17 @@
 #!/usr/bin/node
+// Characters of a Star Wars movie
 const request = require('request');
-const url = 'https://swapi.co/api/films/' + process.argv[2];
-request(url, function (err, response, content) {
-  if (!err) {
-    const characters = JSON.parse(content).characters;
-    characters.forEach((character) => {
-      request(character, function (err, response, content) {
-        if (!err) {
-          console.log(JSON.parse(content).name);
+const url = `https://swapi-api.hbtn.io/api/films/${process.argv[2]}`;
+request(url, 'utf8', (error, response, body) => {
+  if (error) throw error;
+  else {
+    for (const t of JSON.parse(body).characters) {
+      request(t, 'utf8', (error, response, body) => {
+        if (error) throw error;
+        else {
+          console.log(JSON.parse(body).name);
         }
       });
-    });
+    }
   }
 });
